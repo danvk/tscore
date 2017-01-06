1da9f0380a752174b6658e3cf11b1ff36d475edb
Dan Vanderkam <danvk@sidewalklabs.com>
2017-01-06 17:44:04 -0500

    hack hack hack

--- chain.ts
+++ chain.ts
@@ -2,21 +2,47 @@
  * Implementation of underscore/lodash-style chains in TypeScript.
  */
 
+import * as lodash from 'lodash';
+
 interface Action {
   func: (...args: any[]) => any;
   args: IArguments;
 }
 
-class Wrapped<First, T> {
-  private actions: Action[] = [];
-  constructor(private wrapped: First) {}
+abstract class Chainable<T> {
+  abstract value(): T;
 
-  map(func: T => U): Wrapped<First, U> {
-    this.actions.push({func, args: []});
-    return this;
+  map<U>(func: (t: T) => U) {
+    return new Op(func, this);
+  }
+
+  // XXX how to only expose this when T is really T[]?
+  sort() {
+    return new Op<T, T>(xs => lodash.sortBy(xs), this);
   }
 }
 
-function _<T>(t: T): Wrapped<T> {
-  return new Wrapped(t);
+class Value<T> extends Chainable<T> {
+  constructor(private wrapped: T) {
+    super();
+  }
+
+  value() {
+    return this.wrapped;
+  }
 }
+
+class Op<U, V> extends Chainable<V> {
+  constructor(private op: (u: U) => V, private wrapped: Chainable<U>) {
+    super();
+  }
+
+  value() {
+    return this.op(this.wrapped.value());
+  }
+}
+
+function _<T>(t: T): Chainable<T> {
+  return new Value(t);
+}
+
---
+++ lodash_chain_test.ts
@@ -0,0 +1,44 @@
+import * as chai from 'chai';
+import * as _ from 'lodash';
+
+const { expect } = chai;
+
+function expectDeepEqual(a, b) {
+  expect(a).to.deep.equal(b);
+}
+
+describe('chains', () => {
+  it('should chain map', () => {
+    expectDeepEqual(
+      _([1, 2, 3]).map(x => x * x).value(),
+      [1, 4, 9]
+    );
+  });
+
+  it('should chain map/uniq', () => {
+    expectDeepEqual(
+      _([1, 2, 3, 2]).map(x => x * x).uniq().value(),
+      [1, 4, 9]
+    );
+  });
+
+  it('should chain objects', () => {
+    expectDeepEqual(
+      _({a: 1, b: 2, c: 3})
+        .mapValues((x: any) => x * x)
+        .value(),
+      {a: 1, b: 4, c: 9}
+    );
+  });
+
+  it('should pluck with map', () => {
+    expectDeepEqual(
+      _([{a: 2, b: 2}, {a: 2, b: 3}, {a: 1, b: 4}])
+        .map('a')
+        .sort()
+        .uniq()
+        .value(),
+      [1, 2]
+    );
+  });
+});
