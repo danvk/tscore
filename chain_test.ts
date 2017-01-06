2a85d3f5665249eb154c6d39fc9cff8fba76cb66
Dan Vanderkam <danvk@sidewalklabs.com>
2017-01-06 17:20:37 -0500

    chaining test

---
+++ chain_test.ts
@@ -0,0 +1,33 @@
+import * as chai from 'chai';
+import * as _ from 'lodash';
+
+const { expect } = chai;
+
+function expectDeepEqual(a, b) {
+  expect(a).to.deep.equal(b);
+}
+
+describe('chains', () => {
+  it('should chain map', () => {
+    expectDeepEqual(
+      _([1, 2, 3]).map(x => x * x).value(),
+      [1, 4, 9]
+    );
+  });
+
+  it('should chain map/uniq', () => {
+    expectDeepEqual(
+      _([1, 2, 3, 2]).map(x => x * x).uniq().value(),
+      [1, 4, 9]
+    );
+  });
+
+  it('should chain objects', () => {
+    expectDeepEqual(
+      _({a: 1, b: 2, c: 3})
+        .mapValues((x: any) => x * x)
+        .value(),
+      {a: 1, b: 4, c: 9}
+    );
+  });
+});
